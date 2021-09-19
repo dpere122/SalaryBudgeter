18e25c3c74e78bcc5b8dfedef7bd75f37a6d2f67
Daniel <dpere381@fiu.edu>
2021-09-19 13:16:14 -0400

    Added skeleton

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import Routes from "./Routes";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
