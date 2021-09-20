dc3f618b60c59bb27d3c0b1bfd2de09aab7c36d1
Daniel <dpere381@fiu.edu>
2021-09-20 19:43:40 -0400

    Added watermark

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -4,6 +4,9 @@ function Home() {
   return (
     <div className="container">
       <Budgeter />
+      <p className="position-absolute bottom-0 start-50 translate-middle-x">
+        Created by Daniel Perez
+      </p>
     </div>
   );
 }
